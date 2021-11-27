728261bdd318663c9cb1a9c16f36183c923aabbf
olaved <francisco.olaved3@gmail.com>
2021-11-27 19:47:24 -0300

    Usuarios

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const { dbConnection } = require('./database/config');
 // Crear el servidor de express
 const app = express();
 
+// Lectura y parseo del body
+app.use( express.json() );
+
 // Configurar CORS
 app.use( cors() );
 
@@ -16,15 +19,9 @@ dbConnection();
 
 
 // Rutas
-app.get( '/', (req, res) => {
 
-    res.json({
-        ok: true,
-        msg: 'Hola Mundo'
-    });
-
-});
-
+app.use( '/api/usuarios', require('./routes/usuarios') );
+app.use( '/api/login', require('./routes/auth') );
 
 app.listen( process.env.PORT, () => {
     console.log('Servidor corriendo en puerto ' + process.env.PORT );
